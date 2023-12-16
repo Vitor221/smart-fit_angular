2b1eacc6864868dd65263047b4a0020354461e5a
Vitor <vitornascimento6650@gmail.com>
2023-12-15 22:01:47 -0300

    criando a get-units para pegar as unidades

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GetUnitsService } from '../../services/get-units.service';
 
 @Component({
   selector: 'app-forms',
@@ -14,9 +15,11 @@ export class FormsComponent {
   results = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) {}
 
   ngOnInit(): void {
+    this.unitService.getAllUnits().subscribe(data => console.log(data));
+
     this.formGroup = this.formBuilder.group({
       hour: '',
       showUnits: false
