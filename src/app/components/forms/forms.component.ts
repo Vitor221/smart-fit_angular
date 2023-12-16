8d573d0854805b0a0f7d903a2952b9f8ac94b8d6
Vitor <vitornascimento6650@gmail.com>
2023-12-15 22:16:28 -0300

    Filtrar para as unidades abertas

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
+import { Location } from '../../types/location.interface';
 
 @Component({
   selector: 'app-forms',
@@ -12,22 +13,30 @@ import { GetUnitsService } from '../../services/get-units.service';
   styleUrl: './forms.component.scss'
 })
 export class FormsComponent {
-  results = [];
+  results: Location[] = [];
+  filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) {}
 
   ngOnInit(): void {
-    this.unitService.getAllUnits().subscribe(data => console.log(data));
-
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showUnits: false
+      showUnits: true
+    });
+
+    this.unitService.getAllUnits().subscribe(data => {
+      this.results = data.locations;
+      this.filteredResults = data.locations;
     });
   }
 
   onSubmit(): void {
-    console.log(this.formGroup.value);
+    if(!this.formGroup.value.showUnits) {
+      this.filteredResults = this.results.filter(filter => filter.opened === true);
+    } else {
+      this.filteredResults = this.results;
+    }
   }
 
   onClean(): void {
